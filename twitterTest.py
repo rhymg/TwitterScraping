d77618fa08f53680530718a7d8618e653df1010d
rhymg <58017167+rhymg@users.noreply.github.com>
2020-07-23 17:04:40 -0700

    Update twitterTest.py
    Incremented the var round at the end of each round so the program ends after 5 rounds. Remade a check for repetition based on tweet ids.

--- twitterTest.py
+++ twitterTest.py
@@ -3,9 +3,9 @@ from datetime import date
 import time
 
 # The word to search for
-word = 'mcdonald'
+word = 'hi'
 # The file to write to
-f = open("usernameTest.txt", "a")
+f = open("usernameTest.txt", "a+")
 # today's date
 today = date.today()
 # today's date as a string
@@ -23,6 +23,7 @@ tweetCriteria = got.manager.TweetCriteria().setQuerySearch(word).setSince(since)
 lastTweets = got.manager.TweetManager.getTweets(tweetCriteria)
 # Loops through tweets and uses them
 for tweet in lastTweets:
+    print("here is the tweet id: " + tweet.id)
     print(tweet.text + ' BY: ' + tweet.username + '\n')
     if word in tweet.text.lower():
         # if it has the word, it writes it to the file
@@ -38,27 +39,29 @@ while functioning:
     print('ROUND ' + str(round) + '\n')
     repeat = False
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+    lastIDs = [len(lastTweets)]
+    for i in range(len(lastTweets)):
+        lastIDs.append(lastTweets[i].id)
+
     for tweet in tweets:
-        # Check if Tweet has already been tracked
-        for i in range(len(lastTweets)):
-            if tweet.text == lastTweets[i].text:
-                print('THIS IS A REPEAT')
-                tweets.remove(tweet)
-                repeat = True
+    #Check if Tweet has already been tracked
+        if tweet.id in lastIDs:
+            print("This is a repeat")
+            tweets.remove(tweet)
 
     # Loop through with new tweets list
     for tweet in tweets:
-        if len(tweets) == 0:
-            print("All repeats")
-            break
+        # if len(tweets) == 0:
+        #     print("All repeats")
+        #     break
         print(tweet.text + ' BY: ' + tweet.username + '\n')
         # Check if it has the word in it
-        if word in tweet.text.lower():
-            f.write(tweet.username + '\n')
-            print('username added\n')
-
+        f.write(tweet.username + '\n')
+        print('username added\n')
     lastTweets = tweets
     time.sleep(30)
+    round+=1
     if round == 5:
         functioning = False
     
